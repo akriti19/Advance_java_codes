e64bc27af3d834e5595b220877966b08a404aeef
akg807 <80644244+akg807@users.noreply.github.com>
2023-06-13 03:08:06 +0530

    Add files via upload

---
+++ scifaculty.java
@@ -0,0 +1,10 @@
+package fac;
+
+import univ.*;
+
+public class scifaculty{
+	public static void main(String args[]){
+	student s = new student(10021);
+	System.out.println(s.getRegNumber());
+}
+	}
