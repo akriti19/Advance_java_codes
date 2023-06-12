378a6dd144872cf0821125bcc53591a349d9d824
akg807 <80644244+akg807@users.noreply.github.com>
2023-06-13 03:06:20 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,11 @@
+package univ;
+public class student{
+	private int regNumber;
+	public student(int regNumber){
+		this.regNumber = regNumber;
+
+	}
+	public int getRegNumber(){
+		return this.regNumber;
+	}
+}
